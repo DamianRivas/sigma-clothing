c2640d9b62f0139a52d8b861a34e4e3cba0b123a
damianrivas <drivas1993@gmail.com>
2021-07-08 15:50:10 -0700

    Implement shop page, collection-preview, collection-item components

---
+++ src/components/shop/shop.component.jsx
@@ -0,0 +1,25 @@
+import { Component } from "react";
+
+import SHOP_DATA from "./shop.data";
+
+import CollectionPreview from "../collection-preview/collection-preview.component";
+
+export default class ShopPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collections: SHOP_DATA,
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div>
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
+}
