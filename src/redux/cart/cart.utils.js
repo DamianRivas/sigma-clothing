efabcdc061dd784781df59c9947c6f6fdfc9f8ad
damianrivas <drivas1993@gmail.com>
2021-07-10 21:03:56 -0700

    Implement addItem and removeItem reducers for Cart

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -15,3 +15,23 @@ export const addItemToCart = (cartItems, newItem) => {
 
   return [...cartItems, { ...newItem, quantity: 1 }];
 };
+
+/**
+ * Utility function to either remove an item from the cart
+ * @param {Object[]} cartItems - Array containing the current items in the cart
+ * @param {Object} newItem
+ * @returns {Object[]}
+ */
+export const removeItemFromCart = (cartItems, itemToRemove) => {
+  const existingCartItem = cartItems.find(item => item.id === itemToRemove.id);
+
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(item => item.id !== itemToRemove.id);
+  } else {
+    return cartItems.map(item =>
+      item.id === itemToRemove.id
+        ? { ...item, quantity: item.quantity - 1 }
+        : item
+    );
+  }
+};
