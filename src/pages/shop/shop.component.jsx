45cc2f9ec1d4aad9cc2c97956c966877ee39439f
damianrivas <drivas1993@gmail.com>
2021-07-19 19:09:40 -0700

    Implement async redux with redux-thunk. Move shop data to redux store

---
+++ src/pages/shop/shop.component.jsx
@@ -0,0 +1,38 @@
+import { Component } from "react";
+import { Route } from "react-router";
+import { connect } from "react-redux";
+
+import CollectionsOverviewContainer from "../../components/collections-overview/collections-overview.container";
+import CollectionPageContainer from "../collection/collection.container";
+
+import { fetchCollectionsStartAsync } from "../../redux/shop/shop.actions";
+
+class ShopPage extends Component {
+  componentDidMount() {
+    const { fetchCollectionsStartAsync } = this.props;
+    fetchCollectionsStartAsync();
+  }
+
+  render() {
+    const { match } = this.props;
+    return (
+      <div>
+        <Route
+          exact
+          path={`${match.path}`}
+          component={CollectionsOverviewContainer}
+        />
+        <Route
+          path={`${match.path}/:collectionId`}
+          component={CollectionPageContainer}
+        />
+      </div>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  fetchCollectionsStartAsync: () => dispatch(fetchCollectionsStartAsync()),
+});
+
+export default connect(null, mapDispatchToProps)(ShopPage);
